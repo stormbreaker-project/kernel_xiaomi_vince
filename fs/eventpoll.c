9d786e65f3d622b26cc4bf4563b40be7808c8a7e
starlight5234 <starlight5234@protonmail.ch>
2020-10-16 21:50:44 +0200

    fs: eventpoll: Change identifier for eventpoll logging
    In commit 1efeec868f70438ac97bd6fa80a0a558a0ad56ad we introduced snprintf to change the [timerfd] lock to report as/within epollfd
    but this commit introduced in 4.9.239 upstream
    https://android.googlesource.com/kernel/common/+/f2b4a58ad279a232a2f4530dd150d4442ad9475d%5E%21/#F0
    we're changing the name or freeing the lock if it's big
    
    Change identifier for this snprintf as needed
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1277,7 +1277,7 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	}
 
 	snprintf(buf, sizeof(buf), "epoll_%.*s_file:%s",
-		 (int)sizeof(task_comm_buf), task_comm_buf, name);
+		 (int)sizeof(task_comm_buf), task_comm_buf, n.name);
 	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
 	ws = wakeup_source_register(n.name);
 	release_dentry_name_snapshot(&n);
